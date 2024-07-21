5b451c9990c74bf69ddd22ca6a900bd94a679d7a
bayucr <bayfore@gmail.com>
2024-07-21 21:08:06 +0700

    update amek
    menambahkan amek di text

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-4xl text-center">walang.</Text>
+      <Text className="text-4xl text-center">AMEK.</Text>
     </View>
   );
 }
